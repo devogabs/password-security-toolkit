c9e9a023ff191f8c7eeefce2c0529e103e4f4732
devogabs <gabrielbonesso.ctc@gmail.com>
2025-11-22 13:14:37 -0300

    Created __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,27 @@
+# __init__.py
+
+'''
+Password Security Toolkit
+
+This file initializes the password security toolkit package.
+'''
+
+# Public API
+# __all__ = [
+#     "password_checker",
+#     "password_generator",
+#     "cli",
+# ]
+
+# Metadata
+__version__ = "0.0.0"
+__author__ = "devogabs"
+__description__ = "A toolkit for checking password strength and generating secure passwords."
+
+METADATA = {
+
+    "version": __version__,
+    "author": __author__,
+    "description": __description__,
+
+}
